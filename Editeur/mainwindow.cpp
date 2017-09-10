6a2310981f40bf57b83bd095a16bcfe3d209ef56
octonox <victorpetit40@gmail.com>
2017-09-10 14:08:47 +0200

    Ajoute l'UI de l'éditeur

--- Editeur/mainwindow.cpp
+++ Editeur/mainwindow.cpp
@@ -8,6 +8,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     m_canvas = new SfmlCanvas(QSize(400, 300), QPoint(0, 0), ui->centralWidget);
     resize(800, 600);
+
+    connect(ui->actionQuitter, &QAction::triggered, this, &QMainWindow::close); //We use the Qt 5 syntax to connect signals and slots
 }
 
 MainWindow::~MainWindow()
